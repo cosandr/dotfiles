0c0113afda184f5d123bc819893bc1fbaa08df47
Andrei Costescu <andrei@costescu.no>
2020-06-14 00:03:50 +0200

    Tweaks to updates, set mouse sens
    Add pending updates on server to polybar
    Lower mouse sensitivity on startup

--- dot_config/polybar/scripts/executable_go-check-updates.py
+++ dot_config/polybar/scripts/executable_go-check-updates.py
@@ -5,31 +5,36 @@ import json
 import sys
 
 
-SOCK_PATH = "/run/go-check-updates.sock"
-RUN_QUERY = "http://localhost/api?updates"
+SOCK_PATH   = "/run/go-check-updates.sock"
+RUN_QUERY   = "http://localhost/api?updates"
+SERVER_PATH = "/mnt/sshfs/tmp/go-check-updates.json"
+
+
+def updates_str(data: dict) -> str:
+    if not data.get('updates'):
+        return 'No pending updates'
+    ret_str = ''
+    for u in data['updates']:
+        ret_str += f'{u["pkg"]} -> {u["newVer"]}\n'
+    return ret_str.strip()
 
 
 def main():
-    s = subprocess.run(["curl", "--unix-socket", SOCK_PATH, RUN_QUERY], check=True, capture_output=True, text=True)
-    data = json.loads(s.stdout)
-    if not data.get('data'):
-        raise Exception('Empty response')
-    if '--show' in sys.argv:
-        for u in data['data'].get('updates', []):
-            print(f'{u["pkg"]} -> {u["newVer"]}')
-    elif '--notify' in sys.argv:
-        if not data['data'].get('updates'):
-            subprocess.run(["notify-send", "-u", "critical", "No pending updates"])
-            sys.exit(0)
-        send_str = ''
-        for u in data['data']['updates']:
-            send_str += f'{u["pkg"]} -> {u["newVer"]}\n'
-        subprocess.run(["notify-send", "-u", "critical", send_str.strip()])
+    if '--server' in sys.argv:
+        with open(SERVER_PATH, 'r') as f:
+            data = json.load(f)
     else:
-        if not data['data'].get('updates'):
-            print('0')
-        else:
-            print(len(data['data']['updates']))
+        s = subprocess.run(["curl", "--unix-socket", SOCK_PATH, RUN_QUERY], check=True, capture_output=True, text=True)
+        tmp = json.loads(s.stdout)
+        if not tmp.get('data'):
+            raise Exception('Empty response')
+        data = tmp['data']
+    if '--show' in sys.argv:
+        print(updates_str(data))
+    elif '--notify' in sys.argv:
+        subprocess.run(["notify-send", "-u", "critical", updates_str(data)])
+    else:
+        print(len(data.get('updates', [])))
 
 if __name__ == '__main__':
     if '--refresh' in sys.argv:
