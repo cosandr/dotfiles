567926b1a9b72242427f0843fafca7d54b4afb0e
Andrei Costescu <andrei@costescu.no>
2020-06-14 23:56:44 +0200

    Signal based audio toggle

--- dot_config/polybar/scripts/executable_toggle-input.py
+++ dot_config/polybar/scripts/executable_toggle-input.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
-import subprocess
 import re
-import sys
-
+import signal
+import subprocess
 
 # <sink>: <friendly-name>
 # Should only be two entries that toggle when this script runs
-swap_between = {
+SWAP = {
     "alsa_output.usb-FiiO_FiiO_USB_DAC-E07K-01.analog-stereo": "DAC",
     "alsa_output.pci-0000_00_1f.3.analog-stereo": "MB",
 }
+SWAP_KEYS = list(SWAP.keys())
+CURRENT_DEFAULT = ''
+
+
+def update_default():
+    global CURRENT_DEFAULT
+    s = subprocess.run(["pactl", "info"], text=True, capture_output=True, check=True)
+    for line in s.stdout.split('\n'):
+        m = re.search(r'^Default Sink: (\S+)$', line)
+        if m:
+            CURRENT_DEFAULT = m.group(1)
+            break
 
 
 def move_to_sink(sink: str):
@@ -26,45 +37,33 @@ def move_to_sink(sink: str):
         s = subprocess.run(["pactl", "move-sink-input", s_in, sink], capture_output=True, check=True)
 
 
+def swap_default():
+    global CURRENT_DEFAULT
+    # Default is DAC
+    if CURRENT_DEFAULT == SWAP_KEYS[0]:
+        move_to = SWAP_KEYS[1]
+    # Default is motherboard
+    else:
+        move_to = SWAP_KEYS[0]
+    s = subprocess.run(["pactl", "set-default-sink", move_to], capture_output=True, check=True)
+    move_to_sink(move_to)
+    subprocess.run(["notify-send", "-u", "low", f"Output device changed to {SWAP[move_to]}"])
+    CURRENT_DEFAULT = move_to
+
+
+def print_default():
+    print(SWAP.get(CURRENT_DEFAULT, 'N/A'))
+
+
+def handler_swap(signum, frame):
+    update_default()
+    swap_default()
+    print_default()
+
+
 if __name__ == "__main__":
-    try:
-        # Get current default
-        s = subprocess.run(["pactl", "info"], text=True, capture_output=True, check=True)
-        curr_default = ''
-        for line in s.stdout.split('\n'):
-            m = re.search(r'^Default Sink: (\S+)$', line)
-            if m:
-                curr_default = m.group(1)
-                break
-        if not curr_default:
-            raise Exception('Cannot find current default')
-        # Change defaults and current inputs
-        keys = list(swap_between.keys())
-        # Default is DAC
-        if curr_default == keys[0]:
-            move_to = keys[1]
-            curr_name = swap_between[keys[0]]
-        # Default is motherboard
-        else:
-            move_to = keys[0]
-            curr_name = swap_between[keys[1]]
-        if len(sys.argv) > 1 and sys.argv[1] == '--switch':
-            s = subprocess.run(["pactl", "set-default-sink", move_to], capture_output=True, check=True)
-            move_to_sink(move_to)
-            curr_name = swap_between[move_to]
-            s = subprocess.run(["notify-send", "-u", "low", f"Output device changed to {curr_name}"])
-        print(curr_name)
-    except:
-        raise
-        # Do nothing
-        pass
-
-# active_sink=$(pactl list short sinks | grep RUNNING | awk '{ print $2 }')
-
-# if [ "$active_sink" = "$out_one" ]; then
-#     pactl set-default-sink "$out_two"
-#     echo "Changed default and moved inputs to $out_two"
-# else
-#     pactl set-default-sink "$out_one"
-#     echo "Changed default and moved inputs to $out_one"
-# fi
+    signal.signal(signal.SIGHUP, handler_swap)
+    update_default()
+    print_default()
+    while True:
+        signal.pause()
