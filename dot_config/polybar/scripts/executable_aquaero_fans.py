385a9315129c6cd10505876110232f904061e50c
Andrei Costescu <andrei@costescu.no>
2020-06-25 23:15:51 +0200

    Fixed aquaero stats order, tweak polybar order

--- dot_config/polybar/scripts/executable_aquaero_fans.py
+++ dot_config/polybar/scripts/executable_aquaero_fans.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 SHOW_DICT = {
     "fan1": "DDC",
     "fan3": "D5",
@@ -12,15 +14,14 @@ SEP = " - "
 
 try:
     print_entries = []
+    re_fans = re.compile(r'\|?(?P<name>fan\d+)\t(?P<val>\d+)\|?')
     with open(f'/tmp/aquaero.txt', 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        for entry in line.split('|'):
-            e_name, e_val = entry.split('\t')
-            name = SHOW_DICT.get(e_name)
-            if name:
-                rpm = int(e_val)
-                print_entries.append(f'{name}: {rpm:,d}{RPM_SYM}')
+        content = f.read()
+    avail_dict = {m.group('name'): int(m.group('val')) for m in re_fans.finditer(content)}
+    for key, name in SHOW_DICT.items():
+        val = avail_dict.get(key)
+        if val:
+            print_entries.append(f'{name}: {val:,d}{RPM_SYM}')
     if print_entries:
         print(SEP.join(print_entries))
 except:
