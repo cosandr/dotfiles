00b27c66be033e800059889eb75c7e0183b8dca8
Andrei Costescu <andrei@costescu.no>
2020-08-01 15:00:56 +0200

    Script to update server hosts file

---
+++ dot_config/i3/executable_update-localip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SRV_IP="dresrv.local"
+
+# Wait for SSH agent
+while [[ -z $SSH_AUTH_SOCK ]]; do sleep 1; done
+
+for i in {0..4}; do
+    if ping -c 1 -W 1 "$SRV_IP"; then
+        break
+    fi
+done
+
+if [[ $i -ge 4 ]]; then
+    notify-send -a "Update DreSRV hosts" "Could not connect to DreSRV at $SRV_IP"
+    exit 1
+fi
+
+set -e -o pipefail
+
+primary_inet=$(route | grep '^default' | grep -m 1 -o '[^ ]*$')
+all_ips=$(ip -o addr show scope global "$primary_inet" | awk '{gsub(/\/.*/,"",$4); print $4}')
+MY_IPV4=$(awk 'NR==1' <<< "$all_ips")
+
+sed_cmd="sed -i \"s/.*desktop.local$/$MY_IPV4 desktop.local/g\" /etc/hosts"
+
+ssh root@DreSRV "$sed_cmd"
+
+notify-send -a "Update DreSRV hosts" "Updated desktop.local to $MY_IPV4"
