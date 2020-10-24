908d1097fcdad4b14c5fabdef7f1c030329a57e6
Andrei Costescu <andrei@costescu.no>
2020-10-24 11:38:44 +0200

    Auto update prezto

--- run_10-prezto.sh
+++ run_10-prezto.sh
@@ -2,4 +2,7 @@
 
 if [[ ! -d "${HOME}/.zprezto" ]]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
+else
+    cd "${HOME}/.zprezto" || exit 1
+    git pull --recurse-submodules
 fi 
