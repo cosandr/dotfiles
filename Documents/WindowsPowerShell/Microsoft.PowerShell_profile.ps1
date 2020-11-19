587466b87606d92d52ddccfee3f45e3ce676b5ec
Andrei Costescu <andrei@costescu.no>
2020-11-19 01:18:59 +0100

    Function to enable developer PS

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -36,3 +36,8 @@ function which {
         $c
     }
 }
+
+function Set-Dev {
+    Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
+    Enter-VsDevShell -SkipAutomaticLocation 1291dbd5
+}
