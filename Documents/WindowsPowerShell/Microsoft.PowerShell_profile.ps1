540a6692a20453ccaa0382812632ff25a53c26ac
Andrei Costescu <andrei@costescu.no>
2020-11-19 01:11:55 +0100

    More PowerShell improvements
    Simplify system PS config, add some new functions and tweak some existing ones

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,38 @@
+﻿# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+# if (!(Test-Path -Path $PROFILE )) { New-Item -Type File -Path $PROFILE -Force }
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler –Key DownArrow -Function HistorySearchForward
+
+Set-PSReadlineKeyHandler –Key Ctrl+RightArrow -Function ForwardWord
+Set-PSReadlineKeyHandler –Key Ctrl+LeftArrow -Function BackwardWord
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+$HOSTS = "C:\Windows\System32\drivers\etc\hosts"
+$env:EDITOR="vim"
+
+function touch ($file) { Write-Output "" >> "$file" }
+function la { Get-ChildItem -Force $args }
+function ll { Get-ChildItem $args }
+function sudoedit { Start-Process -Verb runAs vim $args }
+function sudo-wt { Start-Process -Verb runAs wt.exe $args }
+function rm-rf { Remove-Item -Recurse -Force $args }
+function st { Start-Process "C:\Program Files\Sublime Text 3\sublime_text.exe" $args }
+function which {
+    $c = Get-Command $args
+    if ($c.CommandType -eq "Application") {
+        $c.Source
+    } elseif ($c.CommandType -eq "Function") {
+        $c.ScriptBlock
+    } else {
+        $c
+    }
+}
