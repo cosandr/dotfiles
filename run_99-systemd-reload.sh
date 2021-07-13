11d402fb7b616282019ba80237372b6e97a30a17
Andrei Costescu <andrei@costescu.no>
2021-07-13 17:04:55 +0200

    Add checks before running daemon-reload

--- run_99-systemd-reload.sh
+++ run_99-systemd-reload.sh
@@ -3,4 +3,10 @@
 # Only run if we have systemctl
 command -v systemctl &>/dev/null || exit 0
 
+# Check if we have config dir
+[[ -d ~/.config/systemd/user ]] || exit 0
+
+# Check for stuff in there
+[[ $(find ~/.config/systemd/user -type f | wc -l) -gt 0 ]] || exit 0
+
 systemctl --user daemon-reload
