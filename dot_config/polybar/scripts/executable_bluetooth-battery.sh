a90de49059791fec8484e7012609d5af10523956
Andrei Costescu <andrei@costescu.no>
2021-11-05 11:02:39 +0100

    Update bluetooth-battery

--- dot_config/polybar/scripts/executable_bluetooth-battery.sh
+++ dot_config/polybar/scripts/executable_bluetooth-battery.sh
@@ -6,10 +6,14 @@ set -uo pipefail
 
 # `bluetoothctl info` version 5.60
 # Battery Percentage: 0x46 (70)
+
+# https://github.com/TheWeirdDev/Bluetooth_Headset_Battery_Level
+# dnf install -y bluez-libs-devel python3-bluez
+# pip3 install --user bluetooth_battery
 ICON_OFF=${ICON_OFF:-""}
 ICON_ON=${ICON_ON:-""}
 
-if ! mac="$(bluetoothctl info | awk '/^Device/ {print $2}' | head -n1 | sed 's/:/_/g')"; then
+if ! mac="$(bluetoothctl info | awk '/^Device/ {print $2}' | head -n1)"; then
     echo "$ICON_OFF"
     exit 0
 fi
@@ -19,9 +23,16 @@ if [ -z "$mac" ]; then
     exit 0
 fi
 
-if perc="$(dbus-send --print-reply=literal --system --dest=org.bluez \
-    /org/bluez/hci0/dev_"$mac" org.freedesktop.DBus.Properties.Get \
-    string:"org.bluez.Battery1" string:"Percentage" 2>/dev/null | grep -oP '\d+$')"; then
+if command -v bluetooth_battery &> /dev/null; then
+    # Battery level for F4:7D:EF:0C:BE:02 is 65%
+    perc="$(bluetooth_battery "$mac" | grep -oP '\d+%$')"
+else
+    perc="$(dbus-send --print-reply=literal --system --dest=org.bluez \
+    /org/bluez/hci0/dev_"${mac//:/_}" org.freedesktop.DBus.Properties.Get \
+    string:"org.bluez.Battery1" string:"Percentage" 2>/dev/null | grep -oP '\d+$')"
+fi
+
+if [[ -n $perc ]]; then
     echo "${ICON_ON}${perc}%"
     exit 0
 else
