5975c9c4fa838dd6cef0ad2eb10c9ad460600185
Andrei Costescu <andrei@costescu.no>
2021-11-18 11:16:15 +0100

    Enable services automatically

--- run_99-systemd-reload.sh
+++ run_99-systemd-reload.sh
@@ -10,3 +10,12 @@ command -v systemctl &>/dev/null || exit 0
 [[ $(find ~/.config/systemd/user -type f | wc -l) -gt 0 ]] || exit 0
 
 systemctl --user daemon-reload
+
+# Ensure services are enabled
+services=( "dresrv-gpg" "dresrv-tunnel" "dresrv-wait-online" "kanshi" "swayidle" "ws-gpg" "ws-proxy" "ws-wait-online" )
+
+for srv in "${services[@]}"; do
+    if [[ -f ~/.config/systemd/user/"$srv".service ]]; then
+        systemctl --user enable "$srv".service
+    fi
+done
