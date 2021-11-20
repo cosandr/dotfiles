dcb621870e15d36b8f42de630761f3c0d18e6f46
Andrei Costescu <andrei@costescu.no>
2021-11-20 21:38:29 +0100

    Remove prom-tunnel and caddy

--- install-caddy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-caddy_path="$HOME/.local/bin/caddy"
-
-if [[ ! -f "$caddy_path" ]]; then
-    curl -L -o "$caddy_path" 'https://caddyserver.com/api/download?os=linux&arch=amd64'
-    chmod 755 "$caddy_path"
-fi
