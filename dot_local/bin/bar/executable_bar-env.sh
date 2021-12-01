bd156d43495b7e8d791e70ccc55e5dec31ad7813
Andrei Costescu <andrei@costescu.no>
2021-12-01 10:16:11 +0100

    Split common bar env into its own file

---
+++ dot_local/bin/bar/executable_bar-env.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Find and set CPU package
+# https://github.com/polybar/polybar/issues/2078
+for i in /sys/class/hwmon/hwmon*/temp*_input; do
+    name="$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))"
+    if [ "$name" = "k10temp: Tctl" ]; then
+        export HWMON_PATH="$i"
+        break
+    elif [ "$name" = "coretemp: Package id 0" ]; then
+        export HWMON_PATH="$i"
+        break
+    fi
+done
+
+# Find and set amdgpu hwmon
+for i in /sys/class/hwmon/hwmon*/device/vendor; do
+    # Found amdgpu
+    if [[ $(<"$i") -eq "0x1002" ]] && [[ -f $(dirname "$i")/gpu_busy_percent ]]; then
+        export HWMON_AMDGPU="$(dirname "$(dirname "$i")")"
+        break
+    fi
+done
+
+# Find and set brightness device
+for i in /sys/class/backlight/*/brightness; do
+    export BACKLIGHT_DEV="$(basename "$(dirname "$i")")"
+    break
+done
+
+# Find and set default ethernet adapter
+if ! PRIMARY_ETH=$(route | grep '^default' | awk '{print $NF}' | grep -m 1 '^e'); then
+    PRIMARY_ETH=""
+fi
+export PRIMARY_ETH
+
+# Find and set default wifi adapter
+if ! PRIMARY_WLAN=$(route | grep '^default' | awk '{print $NF}' | grep -m 1 '^w'); then
+    PRIMARY_WLAN=""
+fi
+export PRIMARY_WLAN
