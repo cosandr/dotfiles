2f4ddcbefc75c57f5039c4c1557b40efc3db8b26
Andrei Costescu <andrei@costescu.no>
2023-01-21 21:54:26 +0100

    Improve docker completion installation scripts

--- run_once_install-docker-completion.sh
+++ run_once_install-docker-completion.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-docker_version=$(docker version -f 'v{{ .Client.Version }}')
-if [[ $? -eq 0 ]]; then
-    echo "Downloading Docker ${docker_version} ZSH completion"
-    mkdir -p ~/.zsh/completion
-    curl -sS \
-        -L "https://raw.githubusercontent.com/docker/cli/${docker_version}/contrib/completion/zsh/_docker" \
-        -o ~/.zsh/completion/_docker
+if command -v docker &>/dev/null; then
+    docker_version=$(docker version -f 'v{{ .Client.Version }}')
+    if [[ -n $docker_version ]]; then
+        echo "Downloading Docker ${docker_version} ZSH completion"
+        mkdir -p ~/.zsh/completion
+        curl -sS \
+            -L "https://raw.githubusercontent.com/docker/cli/${docker_version}/contrib/completion/zsh/_docker" \
+            -o ~/.zsh/completion/_docker
+    fi
 fi
 
-compose_version=$(docker-compose version --short)
-if [[ $? -eq 0 ]]; then
-    echo "Downloading docker-compose ${compose_version} ZSH completion"
-    mkdir -p ~/.zsh/completion
-    curl -sS \
-        -L "https://raw.githubusercontent.com/docker/compose/${compose_version}/contrib/completion/zsh/_docker-compose" \
-        -o ~/.zsh/completion/_docker-compose
+if command -v docker-compose &>/dev/null; then
+    compose_version=$(docker-compose version --short)
+    if [[ -n $compose_version ]]; then
+        echo "Downloading docker-compose ${compose_version} ZSH completion"
+        mkdir -p ~/.zsh/completion
+        curl -sS \
+            -L "https://raw.githubusercontent.com/docker/compose/${compose_version}/contrib/completion/zsh/_docker-compose" \
+            -o ~/.zsh/completion/_docker-compose
+    fi
 fi
