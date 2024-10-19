1a4f26300589d2415578b8ac721bd7ca4e46aa4d
Andrei Costescu <andrei@costescu.no>
2024-10-19 13:58:57 +0300

    Mac fixes
    - Unknown iTerm changes
    - Maybe fix tilde switch

--- run_once_install_tilde-switch.sh
+++ run_once_install_tilde-switch.sh
@@ -5,7 +5,7 @@
 
 set -eu
 
-# https://apple.stackexchange.com/a/353941
+# https://apple.stackexchange.com/a/470622
 echo "Installing launch daemon plist"
 sudo /usr/bin/env bash -c "cat > /Library/LaunchDaemons/org.custom.tilde-switch.plist" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -14,12 +14,15 @@ sudo /usr/bin/env bash -c "cat > /Library/LaunchDaemons/org.custom.tilde-switch.
   <dict>
     <key>Label</key>
     <string>org.custom.tilde-switch</string>
-    <key>Program</key>
-    <string>${HOME}/.local/bin/tilde-switch</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/bin/sudo</string>
+        <string>${HOME}/.local/bin/tilde-switch</string>
+    </array>
     <key>RunAtLoad</key>
     <true/>
     <key>KeepAlive</key>
-    <false/>
+    <true/>
   </dict>
 </plist>
 EOF
